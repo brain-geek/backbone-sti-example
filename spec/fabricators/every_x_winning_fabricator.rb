7f8cbd97a3ad851a51611bf66eaf879be023fa33
Alex Rozumey <brain-geek@yandex.ua>
2012-12-02 19:22:40 +0200

    Add every x wining

---
+++ spec/fabricators/every_x_winning_fabricator.rb
@@ -0,0 +1,7 @@
+Fabricator(:every_x_winning) do
+  date_start { Time.now }
+  date_end { Time.now + 7.days }
+  availible_hours_start 5
+  availible_hours_end 17
+  availibility_weekday_mon true
+end
---
+++ spec/models/every_x_winning_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe EveryXWinning do
+  it_behaves_like "winning"
+end
